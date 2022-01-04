bd275a1b312a0f2b1e479e09445f55e59aa9a00f
AtikaNurfadilah <atikanurfadilah03@gmail.com>
2022-01-05 07:16:12 +0800

    perbaikan lagiii

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <windows.h>
-#include <GL/glut.h> //merapikan
+#include <GL/glut.h> //merapikan (2)
 
     //Initializes 3D rendering (memberikan inisialisai 3D)
     void initRendering() {
